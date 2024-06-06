2d7c6cfec384b580d1eb6341c429a6e6daf2f1c8
Denisson Freitas <freitasdenis_dev@hotmail.com>
2024-06-06 10:54:32 -0300

    Ocultando arquivos com informações sensíveis usando o cypress.env.json

--- cypress/e2e/api-mensagens.cy.js
+++ cypress/e2e/api-mensagens.cy.js
@@ -1,12 +1,11 @@
 // API Test
 describe('API Adopet', () => {
 
-    const authorization = `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJlOGE2NGU4NS1hMzRiLTQ3ZGEtYjY1YS05MzczMDA5MWEyOGEiLCJhZG9wdGVyTmFtZSI6IkRvdWdsYXMgRGVsYXMiLCJpYXQiOjE3MTc2MzA0NzgsImV4cCI6MTcxNzg4OTY3OH0.0fmdPGM1hU567qvOxpRQ6FHtk3a0CEIyw6s0rYLZO38`
     it('Mensagens da API', () => {
         cy.request({
             method: 'GET',
             url: 'https://adopet-api-i8qu.onrender.com/mensagem/e8a64e85-a34b-47da-b65a-93',
-            headers: { authorization }
+            headers: Cypress.env()
         }).then((res) => {
             expect(res.status).to.be.equal(200);
             expect(res.body).is.not.empty;
